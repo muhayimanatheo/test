449ab1d61e65ac8976590279af8e29432a6a4130
muhayimanatheo <115315145+muhayimanatheo@users.noreply.github.com>
2024-05-16 15:58:26 +0200

    changes

--- skills.js
+++ skills.js
@@ -1,14 +1,15 @@
 //1) First to understand the problems
 //array transfomed to string
-const data1 = [17,21,23];
-const data2 = [12,5,-5,0,4];
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
 console.log(`...${data1[0]}ºC... ${data1[1]}ºC...${data1[2]}ºC...`);
-const printForecast = function(arr){
-    let result = '';
-    for(i=0; i<arr.length; i++){
-        result = result + `${arr[i]}ºC in ${i+1} days... `;
-    }
-    console.log('...' + result);
-}
+const printForecast = function (arr) {
+  let result = "";
+  for (i = 0; i < arr.length; i++) {
+    result = result + `${arr[i]}ºC in ${i + 1} days... `;
+  }
+  console.log("..." + result);
+};
 printForecast(data1);
 printForecast(data2);
+ghhhh;
